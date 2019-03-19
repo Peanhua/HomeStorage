147fa994d652a31e86106b7e455ab4d801f3c229
Joni Yrjänä <joyr@netikka.fi>
2019-03-19 11:09:30 +0200

    Add application module, refactor stuff from run.py into the application module, use application module from run.py.

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def root():
-    return render_template("index.html")
+from application import app
 
 if __name__ == "__main__":
     app.run(debug=False)
