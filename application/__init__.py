7699e8352e96c3ba758373dbfbd977c976a68a25
Joni Yrjänä <joyr@netikka.fi>
2019-03-19 21:14:11 +0200

    Take SQLAlchemy into use.

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 
+from flask_sqlaclhemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///homestorage.db"
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
+
 from application import views
+
+
+db.create_all()
