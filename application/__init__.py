f7750f3b31dacdcac6c92a3c82b5cc6e79b959ea
Joni Yrjänä <joyr@netikka.fi>
2019-03-19 11:24:36 +0200

    Refactor views to separate file from application/__init__.py.

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-    return render_template("index.html")
+
+from application import views
