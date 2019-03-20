0250d2a00a0d3df798a5a8ea808fb27e991ff887
Joni Yrjänä <joyr@netikka.fi>
2019-03-20 19:40:25 +0200

    Add the ability to change the name of a product.

--- application/products/views.py
+++ application/products/views.py
@@ -2,11 +2,13 @@ from application import app, db
 from application.products.models import Product
 from flask import redirect, render_template, request, url_for
 
+# List of products
 @app.route("/products", methods=["GET"])
 def products_index():
     return render_template("products/list.html", products = Product.query.all())
 
 
+# Create new product
 @app.route("/products/new/")
 def products_form():
     return render_template("products/new.html")
@@ -18,4 +20,14 @@ def products_create():
     db.session().add(product)
     db.session().commit()
     
+    return redirect(url_for("products_index"))
+
+
+@app.route("/products/<product_id>/", methods=["POST"])
+def products_update(product_id):
+    product = Product.query.get(product_id)
+    product.name = request.form.get("name")
+    print(request.form.get("name"))
+    db.session().commit()
+
     return redirect(url_for("products_index"))
