f10d5dd49d5478375aac002c6c2adc0a2816f099
Joni Yrjänä <joyr@netikka.fi>
2019-04-23 19:43:58 +0300

    Add some spacing between elements in stock add&remove views. Remove unused stock_edit view.

--- application/storages/views.py
+++ application/storages/views.py
@@ -70,36 +70,6 @@ def storages_delete(storage_id):
         return url_for("storages_index")
 
 
-# Stock edit
-@app.route("/storages/<storage_id>/stock", methods=["GET", "POST"])
-@login_required()
-def stock_edit(storage_id):
-    storage = Storage.query.get(storage_id)
-    if not storage:
-        return redirect(url_for("auth_unauthorized"))
-    home = Home.query.get(storage.home_id)
-
-    if not home.is_user_in(current_user.user_id):
-        return redirect(url_for("auth_unauthorized"))
-    
-    def view():
-        products = storage.get_stock()
-        homeproducts = home.get_stock_all()
-        for product in products:
-            hp = next(p for p in homeproducts if p["product_id"] == product["product_id"])
-            product["current_total_quantity"] = hp["current_quantity"]
-        return render_template("storages/stock_edit.html", home=home, storage=storage, products=products)
-
-    if request.method == "GET":
-        return view()
-    
-    elif request.method == "POST":
-        product_ids = request.form.getlist("productid[]")
-        changes     = request.form.getlist("change[]")
-        storage.adjust_stock(product_ids, changes)
-        return view()
-
-
 @app.route("/storages/<storage_id>/add_items", methods=["GET", "POST"])
 @login_required()
 def stock_add(storage_id):
